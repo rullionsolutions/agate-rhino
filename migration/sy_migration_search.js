bfcde878cb3fbae20f0798d8dbb03b17a75b8423
Stephen Francis <stephen.francis@rullion.co.uk>
2017-05-15 18:18:57 +0100

    change search page title for consistency

--- migration/sy_migration_search.js
+++ migration/sy_migration_search.js
@@ -6,7 +6,7 @@ var UI = require("lazuli-ui/index.js");
 module.exports = UI.SearchPage.clone({
     id: "sy_migration_search",
     entity_id: "sy_migration",
-    title: "Migrations",
+    title: "Search for Migrations",
 });
 
 
